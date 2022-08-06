7c21d0736e74ce3b93ee97036aeb2477e9556e41
Martin <mart256@gmail.com>
2022-08-06 10:13:29 +0200

    Initial commit

---
+++ src/main/kotlin/com/codigomorsa/integracion/IntegracionApplication.kt
@@ -0,0 +1,11 @@
+package com.codigomorsa.integracion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntegracionApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntegracionApplication>(*args)
+}
---
+++ src/test/kotlin/com/codigomorsa/integracion/IntegracionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codigomorsa.integracion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntegracionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
